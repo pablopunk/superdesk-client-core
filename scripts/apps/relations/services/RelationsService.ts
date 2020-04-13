00e87c74dc377a3f2df121790fb030159569973c
pablopunk <pablovarela182@gmail.com>
2020-04-13 10:15:48 +0200

    Don't allow ingested items in related fields

--- scripts/apps/relations/services/RelationsService.ts
+++ scripts/apps/relations/services/RelationsService.ts
@@ -1,44 +1,55 @@
 import {zipObject} from 'lodash';
 import {IArticle, IArticleField} from 'superdesk-api';
-import {isPublished, isFetched} from 'apps/archive/utils';
+import {isPublished, isFetched, isIngested} from 'apps/archive/utils';
 
 const RELATED_LINK_KEYS = 3; // links only have _id, type keys and order (and some old ones only _id)
-const isLink = (association) => association != null && Object.keys(association).length <= RELATED_LINK_KEYS;
+const isLink = (association) =>
+    association != null && Object.keys(association).length <= RELATED_LINK_KEYS;
 
 RelationsService.$inject = ['api', '$q'];
 
 export function RelationsService(api, $q) {
     this.getRelatedKeys = function(item: IArticle, fieldId: string) {
         return Object.keys(item.associations || {})
-            .filter((key) => key.startsWith(fieldId) && item.associations[key] != null)
-            .sort((a, b) => item.associations[a].order - item.associations[b].order);
+            .filter(
+                (key) =>
+                    key.startsWith(fieldId) && item.associations[key] != null,
+            )
+            .sort(
+                (a, b) =>
+                    item.associations[a].order - item.associations[b].order,
+            );
     };
 
     this.getRelatedItemsForField = function(item: IArticle, fieldId: string) {
         const relatedItemsKeys = this.getRelatedKeys(item, fieldId);
         const associations = item.associations || {};
 
-        return $q.all(relatedItemsKeys.map((key) => {
-            if (isLink(associations[key])) {
-                return api.find('archive', associations[key]._id);
-            }
+        return $q
+            .all(
+                relatedItemsKeys.map((key) => {
+                    if (isLink(associations[key])) {
+                        return api.find('archive', associations[key]._id);
+                    }
 
-            return $q.when(associations[key]);
-        })).then((values) => zipObject(relatedItemsKeys, values));
+                    return $q.when(associations[key]);
+                }),
+            )
+            .then((values) => zipObject(relatedItemsKeys, values));
     };
 
     this.getDefaultAllowedWorkflows = function() {
         return {
             in_progress: true,
             published: true,
-            fetched: false,
+            ingested: false,
         };
     };
 
     this.itemHasAllowedStatus = function(item: IArticle, field: IArticleField) {
         const ALLOWED_WORKFLOWS = {
             ...this.getDefaultAllowedWorkflows(),
-            ...field?.field_options?.allowed_workflows || {},
+            ...(field?.field_options?.allowed_workflows || {}),
         };
 
         if (ALLOWED_WORKFLOWS.published !== true && isPublished(item)) {
@@ -49,7 +60,10 @@ export function RelationsService(api, $q) {
             return false;
         }
 
-        if (ALLOWED_WORKFLOWS.fetched !== true && isFetched(item)) {
+        if (
+            ALLOWED_WORKFLOWS.ingested !== true &&
+            (isFetched(item) || isIngested(item))
+        ) {
             return false;
         }
 
